930d2cec888ba90c5fb10f41398022329ea5b376
Joshua Acosta <joshuaacosta@Joshuas-MacBook-Pro.local>
2021-04-10 10:49:31 -0700

    centered text in each square

--- myChess/Grid.py
+++ myChess/Grid.py
@@ -2,21 +2,6 @@
 import pygame
 import constants
 
-def create_display(current_pieces, my_font):
-    pygame.init()
-
-    screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
-
-    done = False
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        create_board(screen)
-        show_pieces(screen, current_pieces, my_font)
-        pygame.display.flip()
-
 def create_board(board):
     # draw dark first
     blue = (0, 128, 255)
@@ -36,13 +21,12 @@ def create_board(board):
 
 
 def show_pieces(board, current_locations, text_module):
-
-    # show pawns
     for row in range(constants.NUM_SQUARES_PER_ROW):
         for column in range(constants.NUM_SQUARES_PER_ROW):
             if current_locations[column][row] != 'e':
                 text_surface = text_module.render(current_locations[column][row], False, (0, 0, 0))
+                text_height = text_surface.get_rect().height
                 text_width = text_surface.get_rect().width
-                x_coord = constants.SQUARE_SIZE * row + constants.SQUARE_SIZE/2 - text_width
+                x_coord = constants.SQUARE_SIZE * row + constants.SQUARE_SIZE/2 - text_height / 2
                 y_coord = constants.SQUARE_SIZE * column + constants.SQUARE_SIZE / 2 - text_width / 2
                 board.blit(text_surface, (y_coord, x_coord))
--- myChess/main.py
+++ myChess/main.py
@@ -6,17 +6,28 @@
 import Grid
 import PieceController
 import pygame
+import constants
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pygame.font.init()  # you have to call this at the start
+    pygame.init()
     my_font = pygame.font.SysFont('arial', 30)
 
     Board = PieceController.create_pieces()
 
     PieceController.show_board(Board)
 
-    Grid.create_display(Board, my_font)
+    screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        Grid.create_board(screen)
+        Grid.show_pieces(screen, Board, my_font)
+        pygame.display.flip()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
