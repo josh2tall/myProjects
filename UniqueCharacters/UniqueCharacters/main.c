7ca3f85002451a7dee8316f831b31000e79deba7
Joshua Acosta <joshuaacosta@Joshuas-MacBook-Pro.local>
2021-08-03 19:57:15 -0700

    created algorithm to check if all characters in a string are unique

---
+++ UniqueCharacters/UniqueCharacters/main.c
@@ -0,0 +1,85 @@
+//
+//  main.c
+//  UniqueCharacters
+//
+//  Created by Joshua Acosta on 8/3/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h> //for malloc
+#include <stdint.h>
+#include <stdbool.h>
+
+bool hasAllUniqueCharacters(char * stringToCheck, uint32_t stringSize);
+
+uint32_t getIndex(char characterToCheck);
+
+uint32_t getSizeOfString(char * stringToGetSizeOf);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    char * test = "this work";
+    
+    uint32_t size = getSizeOfString(test);
+    printf("The size is %d \r\n", size);
+    bool result = hasAllUniqueCharacters(test, size);
+    
+    if(result)
+    {
+        printf("the string has all unique characters\r\n");
+    }
+    else
+    {
+        printf("the string does not have all unique characters\r\n");
+    }
+    
+    return 0;
+}
+
+bool hasAllUniqueCharacters(char * stringToCheck, uint32_t stringSize)
+{
+    uint32_t * hashTable = malloc(stringSize * UINT8_MAX);
+    bool allCharactersUnique = true;
+    
+    for(int i = 0; i < UINT8_MAX; i++)
+    {
+        hashTable[i] = UINT32_MAX;
+    }
+    
+    for(uint32_t i = 0; i < stringSize; i++)
+    {
+        uint32_t index = stringToCheck[i];
+    
+        if(hashTable[index] == UINT32_MAX)
+        {
+            hashTable[index] = stringToCheck[i];
+        }
+        else
+        {
+            allCharactersUnique = false;
+        }
+    }
+    return allCharactersUnique; //temporary
+}
+
+uint32_t getSizeOfString(char * stringToGetSizeOf)
+{
+    uint32_t size = 0;
+    bool done = false;
+    uint32_t iterator = 0;
+    while(!done)
+    {
+        if(stringToGetSizeOf[iterator] != '\0')
+        {
+            size++;
+            iterator++;
+        }
+        else
+        {
+            done = true;
+        }
+    }
+    
+    return size;
+}
--- myChess/Validity.py
+++ myChess/Validity.py
@@ -1,5 +1,8 @@
 import constants
 
+black_pawns_first_move = [0 for x in range(constants.NUM_SQUARES_PER_ROW)]
+white_pawns_first_move = [0 for y in range(constants.NUM_SQUARES_PER_ROW)]
+
 def determine_move_validity(source_block, destination_block, board, is_white_turn):
     destination_piece = board[destination_block[0]][destination_block[1]]
     current_piece = board[source_block[0]][source_block[1]]
@@ -11,34 +14,34 @@ def determine_move_validity(source_block, destination_block, board, is_white_tur
         elif not is_white_turn and destination_piece.islower():
             validity = False
 
-    if current_piece == 'k' or current_piece == 'K':
-        valid_moves = get_king_valid_moves(source_block)
-        print("destination", destination_block)
-        if (destination_block[0], destination_block[1]) not in valid_moves:
-            validity = False
-            print("move is not in list of valid moves.")
+    if current_piece != 'e':
+        valid_moves = ()
+        if current_piece == 'k' or current_piece == 'K':
+            valid_moves = get_king_valid_moves(source_block)
 
-    if current_piece == 'r' or current_piece == "R":
-        valid_moves = get_rook_valid_moves(source_block)
-        if (destination_block[0], destination_block[1]) not in valid_moves:
-            validity = False
-            print("move is not in list of valid moves.")
+        if current_piece == 'r' or current_piece == "R":
+            valid_moves = get_rook_valid_moves(source_block)
 
-    if current_piece == 'n' or current_piece == "N":
-        valid_moves = get_knight_valid_moves(source_block)
-        if (destination_block[0], destination_block[1]) not in valid_moves:
-            validity = False
-            print("move is not in list of valid moves.")
+        if current_piece == 'n' or current_piece == "N":
+            valid_moves = get_knight_valid_moves(source_block)
 
-    if current_piece == 'b' or current_piece == "B":
-        valid_moves = get_bishop_valid_moves(source_block, board)
-        print("destination_block ", destination_block)
-        if (destination_block[0], destination_block[1]) not in valid_moves:
-            validity = False
-            print("move is not in list of valid moves.")
+        if current_piece == 'b' or current_piece == "B":
+            valid_moves = get_bishop_valid_moves(source_block, board)
+
+        if current_piece == 'p' or current_piece == 'P':
+            valid_moves = get_pawn_valid_moves(source_block, board)
+
+        if validity:
+            validity = compare_moves(destination_block, valid_moves)
 
     return validity
 
+def compare_moves(destination_block, valid_moves):
+    is_valid = True
+    if (destination_block[0], destination_block[1]) not in valid_moves:
+        is_valid = False
+        print("move is not in list of valid moves.")
+    return is_valid
 
 def determine_piece_validity(new_move, board, is_white_turn):
     validity = True
@@ -48,6 +51,30 @@ def determine_piece_validity(new_move, board, is_white_turn):
         validity = False
 
     return validity
+
+def get_pawn_valid_moves(current_pos, board):
+    global pawns_first_move
+    valid_moves = []
+    column = 0
+    row = 1
+
+    if pawns_first_move[current_pos[column]]:
+        # move up two squares
+        if current_pos[row] - 2 >= 0:
+            valid_moves.append((current_pos[column], current_pos[row] - 2))
+
+        # move up one square
+        if current_pos[row] - 1 >= 0:
+            valid_moves.append((current_pos[column], current_pos[row] - 1))
+
+        if board[current_pos[column]][current_pos[row]] != 'e':
+            # top left
+            if current_pos[column] - 1 >= 0 and current_pos[row] - 1:
+                valid_moves.append((current_pos[column] - 1, current_pos[row] - 1))
+            if current_pos[column] + 1 >= 0 and current_pos[row] - 1:
+                valid_moves.append((current_pos[column] + 1, current_pos[row] - 1))
+
+    return valid_moves
 
 
 def get_king_valid_moves(current_pos):
@@ -163,55 +190,48 @@ def get_bishop_valid_moves(current_pos, board):
         found_blocks = False
 
         # top right
-        if current_pos[row] - iterator >= 0 and \
-                current_pos[column] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and not top_right_done:
+        if get_bishop_direction_validity(constants.BISHOP_TOP_RIGHT_VALIDITY, current_pos, iterator, top_right_done):
             potential_move = [current_pos[column] + iterator, current_pos[row] - iterator]
             list_potential_move = current_pos[column] + iterator, current_pos[row] - iterator
-            piece_at_potential_move = board[potential_move[column]][potential_move[row]]
-            if piece_at_potential_move == 'e':
+            if get_piece_at_potential_move(board, potential_move) == 'e':
                 valid_moves.append(list_potential_move)
-            elif is_same_type(piece_at_potential_move, current_piece):
+            elif is_same_type(get_piece_at_potential_move(board, potential_move), current_piece):
                 top_right_done = True
             else:
                 valid_moves.append(list_potential_move)
                 top_right_done = True
             found_blocks = True
         # top left
-        if current_pos[row] - iterator >= 0 and current_pos[column] - iterator >= 0 and not top_left_done:
+        if get_bishop_direction_validity(constants.BISHOP_TOP_LEFT_VALIDITY, current_pos, iterator, top_left_done):
             potential_move = [current_pos[column] - iterator, current_pos[row] - iterator]
             list_potential_move = current_pos[column] - iterator, current_pos[row] - iterator
-            piece_at_potential_move = board[potential_move[column]][potential_move[row]]
-            if piece_at_potential_move == 'e':
+            if get_piece_at_potential_move(board, potential_move) == 'e':
                 valid_moves.append(list_potential_move)
-            elif is_same_type(piece_at_potential_move, current_piece):
+            elif is_same_type(get_piece_at_potential_move(board, potential_move), current_piece):
                 top_left_done = True
             else:
                 valid_moves.append(list_potential_move)
                 top_left_done = True
             found_blocks = True
         # bottom right
-        if current_pos[row] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and \
-                current_pos[column] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and not bottom_right_done:
+        if get_bishop_direction_validity(constants.BISHOP_BOTTOM_RIGHT_VALIDITY, current_pos, iterator, bottom_right_done):
             potential_move = [current_pos[column] + iterator, current_pos[row] + iterator]
             list_potential_move = current_pos[column] + iterator, current_pos[row] + iterator
-            piece_at_potential_move = board[potential_move[column]][potential_move[row]]
-            if piece_at_potential_move == 'e':
+            if get_piece_at_potential_move(board, potential_move) == 'e':
                 valid_moves.append(list_potential_move)
-            elif is_same_type(piece_at_potential_move, current_piece):
+            elif is_same_type(get_piece_at_potential_move(board, potential_move), current_piece):
                 bottom_right_done = True
             else:
                 valid_moves.append(list_potential_move)
                 bottom_right_done = True
             found_blocks = True
         # bottom left
-        if current_pos[row] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and \
-                current_pos[column] - iterator >= 0 and not bottom_left_done:
+        if get_bishop_direction_validity(constants.BISHOP_BOTTOM_LEFT_VALIDITY, current_pos, iterator, bottom_left_done):
             potential_move = [current_pos[column] - iterator, current_pos[row] + iterator]
             list_potential_move = current_pos[column] - iterator, current_pos[row] + iterator
-            piece_at_potential_move = board[potential_move[column]][potential_move[row]]
-            if piece_at_potential_move == 'e':
+            if get_piece_at_potential_move(board, potential_move) == 'e':
                 valid_moves.append(list_potential_move)
-            elif is_same_type(piece_at_potential_move, current_piece):
+            elif is_same_type(get_piece_at_potential_move(board, potential_move), current_piece):
                 bottom_left_done = True
             else:
                 valid_moves.append(list_potential_move)
@@ -226,6 +246,32 @@ def get_bishop_valid_moves(current_pos, board):
 
     print("bishop valid moves", valid_moves)
     return valid_moves
+
+def get_bishop_direction_validity(direction, current_pos, iterator, done):
+    column = 0
+    row = 1
+    result = False
+
+    if done:
+        return False
+
+    if direction == constants.BISHOP_TOP_LEFT_VALIDITY:
+        result = current_pos[row] - iterator >= 0 and current_pos[column] - iterator >= 0 and not done
+    elif direction == constants.BISHOP_TOP_RIGHT_VALIDITY:
+        result = current_pos[row] - iterator >= 0 and current_pos[column] + iterator <= constants.NUM_SQUARES_PER_ROW - 1
+    elif direction == constants.BISHOP_BOTTOM_LEFT_VALIDITY:
+        result = current_pos[row] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and \
+                    current_pos[column] - iterator >= 0
+    elif direction == constants.BISHOP_BOTTOM_RIGHT_VALIDITY:
+        result = current_pos[row] + iterator <= constants.NUM_SQUARES_PER_ROW - 1 and \
+                    current_pos[column] + iterator <= constants.NUM_SQUARES_PER_ROW - 1
+
+    return result
+
+def get_piece_at_potential_move(board, potential_move):
+    column = 0
+    row = 1
+    return board[potential_move[column]][potential_move[row]]
 
 def is_same_type(piece_a, piece_b):
     return (piece_a.islower() and piece_b.islower()) or (piece_a.isupper() and piece_b.isupper())
