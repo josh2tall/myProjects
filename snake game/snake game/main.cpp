7000db30354b7add0cd05b39efc22a62ac346a4c
josh2tall <acosta.joshua@hotmail.com>
2022-06-10 11:04:21 -0700

    working snake game

--- snake game/snake game/main.cpp
+++ snake game/snake game/main.cpp
@@ -11,18 +11,10 @@
 #include <time.h>
 
 #include "constants.h"
-#include "neural_network.h"
 #include "snake_game_core.h"
-
-void forMachineLearningTest(char * map){
-    //get surrounding values
-    const char possibleMoves[] = {'N','S','W','E'};
-    //
-}
 
 int main(int argc, const char * argv[]) {
     srand(time(0));
-    printSomething();
     int * snakePos = getSnakePositions();
     for(int i = 0; i < winningLength + 1; i++){
         snakePos[i] = -1;
